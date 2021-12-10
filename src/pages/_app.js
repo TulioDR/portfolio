520a2d68142da6f50cddf957decbd3777dfda0c6
Tulio <tulioruzo29@gmail.com>
2021-12-10 16:21:10 -0400

    image name fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import "tailwindcss/tailwind.css";
 import "styles/custom.css";
-
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,7 +7,6 @@ import "aos/dist/aos.css";
 function MyApp({ Component, pageProps }) {
    useEffect(() => {
       AOS.init({
-         // offset: 400, // offset (in px) from the original trigger point
          duration: 1500,
          once: true,
       });
